c9591db22988e3c353f7a09b153b13654372bb35
Mikael Kontolampi <mikael.kontolampi@gmail.com>
2024-01-01 21:10:18 +0100

    Modify Home page hero image, texts and button

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,22 +1,68 @@
 import styled from "styled-components";
+import Hero from "../../assets/hero.jpg";
+import { Link } from "react-router-dom";
 
 const StyledHome = styled.div`
   flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
   padding: 12px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)),
+    /* Dark layer with 85% opacity */ url(${Hero}); /* Your background image */
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+  color: white;
 `;
 
-const Title = styled.h1`
-  background: #f6f6f6;
-  padding: 12px;
-  margin: 8px;
+const StyledContainer = styled.div`
+  width: 350px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledH1 = styled.h1`
+  font-size: 50px;
+  margin: 0;
+`;
+
+const BlueButton = styled.button`
+  background-color: skyblue;
+  padding: 8px 12px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: #6ca5bc;
+  }
 `;
 
 const Home = () => {
   return (
-    <>
-      <Title>Home</Title>
-      <StyledHome></StyledHome>
-    </>
+    <StyledHome>
+      <StyledContainer>
+        <StyledH1>Welcome to Webshop!</StyledH1>
+      </StyledContainer>
+      <StyledContainer>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Cras
+          pulvinar mattis nunc sed.
+        </p>
+      </StyledContainer>
+      <StyledContainer>
+        <Link to="/products">
+          <BlueButton>Shop now</BlueButton>
+        </Link>
+      </StyledContainer>
+    </StyledHome>
   );
 };
 
